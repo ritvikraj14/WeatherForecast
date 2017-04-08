0a5d87aa9079b7c36817d9d2e920a1b55aca5552
Ritvik Raj <ritvikraj13@gmail.com>
2017-04-08 16:21:02 +0530

    Add files via upload

---
+++ WeatherForecast_WebScrapping.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+page = requests.get("http://forecast.weather.gov/MapClick.php?lat=37.7772&lon=-122.4168")
+soup = BeautifulSoup(page.content, 'html.parser')
+seven_day = soup.find(id="seven-day-forecast")
+forecast_items = seven_day.find_all(class_="tombstone-container")
+short_descs = [sd.get_text() for sd in seven_day.select(".tombstone-container .short-desc")]
+temps = [t.get_text() for t in seven_day.select(".tombstone-container .temp")]
+descs = [d["title"] for d in seven_day.select(".tombstone-container img")]
+weather = pd.DataFrame({
+        "period": periods, 
+        "short_desc": short_descs, 
+        "temp": temps, 
+        "desc": descs
+    })
+
+weather
